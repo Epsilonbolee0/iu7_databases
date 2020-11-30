2d2d5e01f6fea55c55ef6bc1f85f1abdda762077
nick <pavlov.nick.2000@gmail.com>
2020-12-01 01:55:51 +0300

    fixed lab_3

--- lab_3/functions/multioperator_table_function.sql
+++ lab_3/functions/multioperator_table_function.sql
@@ -1,6 +1,24 @@
-CREATE OR REPLACE FUNCTION get_comment_summary(int, int, date, char) RETURNS TABLE 
+-- Создание лога по сообщениям на текущий момент
+
+CREATE TEMP TABLE IF NOT EXISTS commentary_summary (
+	id          INT,
+	summ_time	TIMESTAMP,		
+	email       VARCHAR,
+	name        VARCHAR,
+	sex   	    CHAR,
+	birth_date  DATE,
+	task_text   TEXT,
+	solution    TEXT,
+	task_rating SMALLINT,
+	comment     TEXT,
+	rating      SMALLINT,
+	PRIMARY KEY(id, summ_time)
+);
+
+CREATE OR REPLACE FUNCTION get_comment_summary(int) RETURNS TABLE 
 (
 	id          INT,
+	summ_time 	TIMESTAMP,
 	email       VARCHAR,
 	name        VARCHAR,
 	sex   	    CHAR,
@@ -12,7 +30,10 @@ CREATE OR REPLACE FUNCTION get_comment_summary(int, int, date, char) RETURNS TAB
 	rating      SMALLINT
 ) AS 
 $$
-	SELECT commentary.id,
+	INSERT INTO commentary_summary
+	SELECT 
+		   commentary.id,
+		   current_timestamp AS summ_date,
 		   email,
 		   accounts.name,
 		   sex,
@@ -24,11 +45,10 @@ $$
 		   commentary.rating
 	FROM commentary JOIN accounts ON 
 	commentary.author_id = accounts.id
-	JOIN tasks ON tasks.id = task_id AND commentary.id = $1
-	WHERE tasks.rating >= $2 AND accounts.birth_date <= $3 AND accounts.sex = $4;
-
+	JOIN tasks ON tasks.id = task_id AND commentary.id = $1;
+	
+	SELECT * FROM commentary_summary
 $$  LANGUAGE SQL;
 
-SELECT * FROM get_comment_summary(22);
-
+SELECT * FROM get_comment_summary(2);
 
--- lab_3/procedures/metadata_procedure.sql
+++ lab_3/procedures/metadata_procedure.sql
@@ -1,15 +1,12 @@
 -- Получить названия всех ограничений в столбцах и таблицах
 
-SELECT * 
-	FROM (
-		SELECT table_name, column_name, constraint_name
-		FROM information_schema.constraint_column_usage
-		WHERE constraint_schema NOT IN ('information_schema', 'pg_catalog')
-	) AS tmp;
-
 CREATE OR REPLACE PROCEDURE get_constraint_data() AS $$
+DECLARE
+	row RECORD;
 BEGIN
-	FOR row IN tmp
+	FOR row IN (SELECT table_name, column_name, constraint_name
+	 			FROM information_schema.constraint_column_usage
+				WHERE constraint_schema NOT IN ('information_schema', 'pg_catalog'))
 	LOOP
 		RAISE NOTICE '{table: %} {column: %} {constraint: %}', row.table_name, row.column_name, row.constraint_name;
 	END LOOP;
